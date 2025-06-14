b0a8930ebe26eaa32faea1e9848b105588cf47d4
Kaushik Raj Panta <krp@logpoint.com>
2025-06-14 10:16:32 +0545

    Program 5 is added.

---
+++ Basic Output and Variables/5/main.go
@@ -0,0 +1,16 @@
+// Check if a number is even or odd.
+
+package main
+
+import "fmt"
+
+func main() {
+	num := 10
+
+	if num%2 == 0 {
+		fmt.Println("Number is Even.")
+	} else {
+		fmt.Println("Number is Odd.")
+	}
+
+}
