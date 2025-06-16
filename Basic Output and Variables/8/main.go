c2c1eadc312105e2ff21db5940af691b07ccc9f5
Kaushik Raj Panta <krp@logpoint.com>
2025-06-16 16:17:57 +0545

    Program 8 is added.

---
+++ Basic Output and Variables/8/main.go
@@ -0,0 +1,11 @@
+// Convert Celsius to Fahrenheit.
+
+package main
+
+import "fmt"
+
+func main() {
+	celsius_value := 37.0
+	fahrenheit_value := 1.8*celsius_value + 32
+	fmt.Printf("Value is : %.2f", fahrenheit_value)
+}
