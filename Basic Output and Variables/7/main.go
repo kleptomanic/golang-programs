074c63a467f15d38861c5de90b1b0db47402764d
Kaushik Raj Panta <krp@logpoint.com>
2025-06-16 16:28:18 +0545

    Program 4 code refactored using single line func

--- Basic Output and Variables/7/main.go
+++ Basic Output and Variables/7/main.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	length, breadth := 10, 10
-	area_of_rectangle := length * breadth
-	fmt.Printf("Area of Rectangle of length %d and breadth %d is %d", length, breadth, area_of_rectangle)
+	fmt.Println("Area of rectangle : ", func(l, b int) int {
+		return l * b
+	}(3, 2))
 }
