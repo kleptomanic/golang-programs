bb6a2fbdf530f11b256d27b8455bfd7faf5c1de3
Kaushik Raj Panta <krp@logpoint.com>
2025-06-16 15:24:04 +0545

    Program 7 is added.

---
+++ Basic Output and Variables/7/main.go
@@ -0,0 +1,11 @@
+// Calculate the area of a rectangle.
+
+package main
+
+import "fmt"
+
+func main() {
+	length, breadth := 10, 10
+	area_of_rectangle := length * breadth
+	fmt.Printf("Area of Rectangle of length %d and breadth %d is %d", length, breadth, area_of_rectangle)
+}
