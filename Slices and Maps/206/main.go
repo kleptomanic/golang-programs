d32ba764e50bc7bdbe029953d0bcc099577bd83f
Kaushik Raj Panta <krp@logpoint.com>
2025-06-20 09:14:39 +0545

    Program 206 is added.

---
+++ Slices and Maps/206/main.go
@@ -0,0 +1,25 @@
+// Reverse a slice of integers.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	list := []int{1, 2, 3, 4, 5, 6}
+
+	// slices.Reverse(list)
+
+	// Old style
+	left := 0
+	right := len(list) - 1
+	for left < right {
+		temp := list[left]
+		list[left] = list[right]
+		list[right] = temp
+		left++
+		right--
+	}
+	fmt.Println(list)
+}
